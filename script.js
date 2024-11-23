a68365e0c4c20461c73ae272dfbee0c6a138e631
trix04 <jxt_04@outlook.com>
2024-11-22 22:42:54 -0500

    Edited all files to fix structure and style of 'focus & blur' events.

--- script.js
+++ script.js
@@ -10,12 +10,15 @@ hoverButton.addEventListener('mouseleave', () => {
 });
 
 
+
 let keyboardInput = document.getElementById('keyboardInput');
 let keyPressedMessage = document.getElementById('keyPressedMessage');
 
 keyboardInput.addEventListener('keydown', (event) => {
     keyPressedMessage.textContent = `Latest key pressed: ${event.key}`;
 });
+
+
 
 document.addEventListener('DOMContentLoaded', function() {
     let form = document.getElementById('myForm');
@@ -38,6 +41,8 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+
+
 function handleFocus() {
     document.getElementById('skyInput').style.background = 'lightblue';
 }
@@ -45,7 +50,6 @@ function handleFocus() {
 function validateAge() {
     var age = document.getElementById('ageInput');
     var ageValue = parseInt(age.value, 10);
-
     
     if (ageValue >= 18) {
         age.style.backgroundColor = 'lightgreen';
@@ -55,6 +59,26 @@ function validateAge() {
         console.log('Check age, may be under 18.');
     }
 }
+
+let ageInpput = document.getElementById('ageInput');
+let ageMessage = document.getElementById('ageMessage');
+
+ageInput.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+
+        event.preventDefault();
+       
+        let ageValue = parseInt(ageInput.value, 10);
+        if (ageValue >= 18) {
+            ageMessage.textContent = 'Eligible age confirmed.';
+        } else {
+            ageMessage.textContent = 'Check age eligibility, may be under 18.';
+        }
+    }
+});
+
+
+
 
 let buttonContainer = document.getElementById('buttonContainer');
 let delegationMessage = document.getElementById('delegationMessage');
