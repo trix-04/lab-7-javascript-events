f7529e8910e041cd2f190646ac18a4ec480bc80d
trix04 <jxt_04@outlook.com>
2024-11-22 21:06:26 -0500

    Edited JS file so for 'form event' function.

--- script.js
+++ script.js
@@ -16,3 +16,24 @@ let keyPressedMessage = document.getElementById('keyPressedMessage');
 keyboardInput.addEventListener('keydown', (event) => {
     keyPressedMessage.textContent = `Latest key pressed: ${event.key}`;
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    let form = document.getElementById('myForm');
+    let username = document.getElementById('username');
+    let formMessage = document.getElementById('formMessage');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        let userValue = username.value;
+
+        if (userValue.trim() == '') {
+            formMessage.textContent = "Please enter a username.";
+        } else {
+            formMessage.textContent = `Form submitted successfully! Username: ${userValue}`;
+        }
+    });
+
+    username.addEventListener('input', function() {
+        console.log('Input changed: ', this.value);
+    });
+});
